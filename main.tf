b8cb3e82b33cb8dc4ee355e26fc38beee802d2c8
Andrii Cherkasov <acherkas@cisco.com>
2019-07-04 15:17:33 +0300

    - added keepalive - added keepalive timeout - corrected error message

--- main.tf
+++ main.tf
@@ -8,6 +8,8 @@ data "ssh_tunnel" "consul" {
   ssh_agent       = false // by default, SSH agent authentication is attempted if the SSH_AUTH_SOCK environment variable is set
   local_address   = "localhost:0" // use port 0 to request an ephemeral port (a random port)
   remote_address  = "localhost:8500"
+  #keepalive = true // by default the keepalive is enabled
+  #keepalive_interval = 2
 }
 
 provider "consul" {
