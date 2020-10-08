bd2f8ce0c11a1b6dfa131222343b3fbd7bc7a079
Alejandro Torres <fulgorek@gmail.com>
2020-10-08 07:42:02 -0500

    terraform 11.14 support

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/stefansundin/terraform-provider-ssh
 
+go 1.14
+
 require (
 	github.com/hashicorp/terraform v0.11.14
-	golang.org/x/crypto v0.0.0-20180211211603-9de5f2eaf759
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 )
--- main.tf
+++ main.tf
@@ -1,34 +1,11 @@
 provider "ssh" {}
 
-data "ssh_tunnel" "consul" {
-  user            = "stefan" // Optional. If not set, your local user's username will be used.
-  host            = "bastion.example.com"
-  private_key     = "${file(pathexpand("~/.ssh/id_rsa"))}"
-  # certificate     = "${file(pathexpand("~/.ssh/id_rsa-cert.pub"))}" // if you need to use a certificate
-  ssh_agent       = false // by default, SSH agent authentication is attempted if the SSH_AUTH_SOCK environment variable is set
-  local_address   = "localhost:0" // use port 0 to request an ephemeral port (a random port)
-  remote_address  = "localhost:8500"
-}
-
-provider "consul" {
-  version    = "~> 1.0"
-  address    = "${data.ssh_tunnel.consul.local_address}"
-  scheme     = "http"
-}
-
-data "consul_keys" "keys" {
-  key {
-    name = "revision"
-    path = "revision"
+resource "null_resource" "vagrant" {
+  triggers = {
+    always_run = "${timestamp()}"
   }
-}
 
-output "local_address" {
-  value = "${data.ssh_tunnel.consul.local_address}"
-}
-output "random_port" {
-  value = "${data.ssh_tunnel.consul.port}"
-}
-output "revision" {
-  value = "${data.consul_keys.keys.var.revision}"
+  provisioner "local-exec" {
+    command = "screen -dm ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i .vagrant/machines/default/virtualbox/private_key -p 2201 -N -L 3307:127.0.0.1:3306 vagrant@127.0.0.1 sleep 60"
+  }
 }
